f1f9055652f1c2fe068695e48e54ab70fcbd6ab4
RaymondMcGuire <raymondmg@foxmail.com>
2020-04-22 19:07:00 +0900

    fix SPH viscosity calculation

--- src/sph/sph.ts
+++ src/sph/sph.ts
@@ -2,7 +2,7 @@
  * @Author: Xu.Wang
  * @Date: 2020-03-31 15:12:02
  * @Last Modified by: Xu.Wang
- * @Last Modified time: 2020-04-16 18:39:31
+ * @Last Modified time: 2020-04-22 19:03:34
  */
 import { ParticleSystem } from '../physics/particle_system'
 import { SphData } from './sph_data'
@@ -10,19 +10,19 @@ import { square } from '../math/math_utils'
 import { SphKernelSpiky } from './sph_kernel'
 import { PHY_WATER_DENSITY } from '../physics'
 import { LOG } from '../log/log'
-import { Vector3 } from 'tsmathlib/src/math'
+import { Vector3 } from '../math'
 
 export class SPH extends ParticleSystem {
   _viscosityCoefficient: number = 0.002
   _gasCof: number = 5.0
 
   constructor(
-      mass: number = 0.04,
-      pInKernel :number = 20,
+    mass: number = 0.04,
+    pInKernel: number = 20,
     targetDensity: number = PHY_WATER_DENSITY
   ) {
     super(0.0, mass)
-    let sphParticles = new SphData(mass,pInKernel, targetDensity)
+    let sphParticles = new SphData(mass, pInKernel, targetDensity)
     this.setParticleSystemData(sphParticles)
   }
 
@@ -48,8 +48,8 @@ export class SPH extends ParticleSystem {
     this.calcViscosityForce()
   }
 
-    calcPressureForce() {     
-        this.computePressure()
+  calcPressureForce() {
+    this.computePressure()
 
     let particles = this.sphData()
     let p = particles.positions()
@@ -58,29 +58,26 @@ export class SPH extends ParticleSystem {
     let f = particles.forces()
     let numberOfParticles = particles.numberOfParticles()
 
-
     let massSquared = square(particles.particleMass())
     let kernel = new SphKernelSpiky(particles.kernelRadius())
 
-        for (let i = 0; i < numberOfParticles; i++) {
-
-            let neighbors: Array<number> = particles.neighborLists()[i]
-         // waste my 8hours
-            for (let idx = 0; idx < neighbors.length; idx++) {
-                let j = neighbors[idx]
-            let dist = p[i].distanceTo(p[j])
-            if (dist > 0.0) {
-                let dir = (p[j].sub(p[i])).div(dist)
-                let tmpFCoef =  (pr[i] / (d[i] * d[i]) + pr[j] / (d[j] * d[j]))*massSquared
-                let tmpF = kernel.getGradientByDistance(dist, dir).mul(tmpFCoef)
-                f[i] = f[i].sub(tmpF)
-
-            }
+    for (let i = 0; i < numberOfParticles; i++) {
+      let neighbors: Array<number> = particles.neighborLists()[i]
+      // waste my 8hours
+      for (let idx = 0; idx < neighbors.length; idx++) {
+        let j = neighbors[idx]
+        let dist = p[i].distanceTo(p[j])
+        if (dist > 0.0) {
+          let dir = p[j].sub(p[i]).div(dist)
+          let tmpFCoef =
+            (pr[i] / (d[i] * d[i]) + pr[j] / (d[j] * d[j])) * massSquared
+          let tmpF = kernel.getGradientByDistance(dist, dir).mul(tmpFCoef)
+          f[i] = f[i].sub(tmpF)
         }
-        
-        // console.log(f[i].x(),f[i].y(),f[i].z())
+      }
+
+      // console.log(f[i].x(),f[i].y(),f[i].z())
     }
-        
   }
 
   calcViscosityForce() {
@@ -96,53 +93,49 @@ export class SPH extends ParticleSystem {
 
     for (let i = 0; i < numberOfParticles; i++) {
       let neighbors: Array<number> = particles.neighborLists()[i]
-      for (const j in neighbors) {
+      for (let idx = 0; idx < neighbors.length; idx++) {
+        let j = neighbors[idx]
         let dist = x[i].distanceTo(x[j])
-        f[i] = f[i].add(
-          v[j]
-            .sub(v[i])
-            .div(d[j])
-            .mul(
-              kernel.getSecondDerivative(dist) *
-                massSquared *
-                this.viscosityCoefficient()
-            )
-        )
+        let tmpVCoef =
+          kernel.getSecondDerivative(dist) *
+          massSquared *
+          this.viscosityCoefficient()
+        let tmpV = v[j].sub(v[i]).div(d[j]).mul(tmpVCoef)
+        f[i] = f[i].add(tmpV)
       }
     }
   }
-    
-    update() {
-        
-        let particles = this.sphData();
+
+  update() {
+    let particles = this.sphData()
 
     // clear forces
-        let forces = particles.forces()
-        for (let idx = 0; idx < particles.numberOfParticles(); idx++) {
-            forces[idx] = new Vector3()
-        }
+    let forces = particles.forces()
+    for (let idx = 0; idx < particles.numberOfParticles(); idx++) {
+      forces[idx] = new Vector3()
+    }
 
     // update collider
 
-    this.allocateBuffers();
+    this.allocateBuffers()
 
     // build neighbor searcher
     particles.buildSphNeighborSearcher()
     particles.buildSphNeighborLists()
 
     // update density
-    particles.calcDensities();
+    particles.calcDensities()
 
-    this.calcForces();
+    this.calcForces()
 
     // time
-    this.timeIntegration(0.005);
+    this.timeIntegration(0.005)
 
-    this.resolveCollision();
+    this.resolveCollision()
 
-    this.updateParticlesData();
-    }
-    
+    this.updateParticlesData()
+  }
+
   computePressure() {
     let particles = this.sphData()
     let targetDensity = particles.targetDensity()
@@ -151,8 +144,7 @@ export class SPH extends ParticleSystem {
     let p = particles.pressures()
 
     for (let i = 0; i < numberOfParticles; i++) {
-        p[i] = (d[i] - targetDensity) * this._gasCof
-        // console.log(p[i])
+      p[i] = (d[i] - targetDensity) * this._gasCof
     }
 
     // not good for real-time SPH
